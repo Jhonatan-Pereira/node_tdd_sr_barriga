5deb102099cdf4fda389b9d0503d045b94b7654f
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-01-31 18:42:53 -0300

    feat: Não deve inserir usuário sem nome

--- src/services/user.js
+++ src/services/user.js
@@ -4,6 +4,11 @@ module.exports = (app) => {
   }
 
   const save = (user) => {
+
+    if(!user.name) {
+      return { error: 'Nome é um atributo obrigatório' }
+    }
+
     return app.db('users').insert(user, '*')
   }
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -2,7 +2,7 @@ const request = require('supertest')
 
 const app = require('../../src/app')
 
-test('Deve lista todos os usuários', () => {
+test('Deve listar todos os usuários', () => {
   return request(app).get('/users')
     .then(res => {
       expect(res.status).toBe(200)
@@ -17,5 +17,14 @@ test('Deve inserir usuário com sucesso', () => {
     .then(res => {
       expect(res.status).toBe(201)
       expect(res.body.name).toBe('Walter Mitty')
+    })
+})
+
+test('Não deve inserir usuário sem nome', () => {
+  return request(app).post('/users')
+    .send({ mail: 'teste@teste.com', passwd: '12345' })
+    .then(res => {
+      expect(res.status).toBe(400)
+      expect(res.body.error).toBe('Nome é um atributo obrigatório')
     })
 })
