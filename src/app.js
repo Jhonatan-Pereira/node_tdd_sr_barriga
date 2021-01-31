41f0d9d422f13e16b0104076c9e99b1562d69909
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-01-31 18:22:07 -0300

    feat: Devo logar consultas

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ const app = require('express')();
 const consign = require('consign')
 const knex = require('knex')
 const knexfile = require('../knexfile')
+const knexLogger = require('knex-logger')
 
 // TODO: criar chaveamento dinâmico
 app.db = knex(knexfile.test)
+
+// DEBUG: log das consultas sql
+// app.use(knexLogger(app.db))
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
@@ -16,6 +20,11 @@ app.get('/', (req, res) => {
   res.status(200).send()
 })
 
-
+// DEBUG: log das consultas sql
+// app.db.on('query', (query) => {
+//   console.log({ sql: query.sql, bindings: query.bindings? query.bindings.join(',') : ''})
+// })
+// .on('query-response', (response) => console.log(response))
+// .on('error', error => console.log(error))
 
 module.exports = app;
