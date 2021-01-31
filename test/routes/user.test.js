d6c445eba14e48163f2307d3416fca0d4c9cbe81
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-01-30 21:39:59 -0300

    refactor: Devo refatorar para usar banco de dados

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,19 +1,19 @@
 const request = require('supertest')
 
-const app = require('../src/app')
+const app = require('../../src/app')
 
 test('Deve lista todos os usuários', () => {
   return request(app).get('/users')
     .then(res => {
       expect(res.status).toBe(200)
-      expect(res.body).toHaveLength(1)
-      expect(res.body[0]).toHaveProperty('name', 'John Doe')
+      expect(res.body.length).toBeGreaterThan(0)
     })
 })
 
 test('Deve inserir usuário com sucesso', () => {
+  const mail = `${Date.now()}@mail.com`
   return request(app).post('/users')
-    .send({ name: 'Walter Mitty', mail: 'water@mail.com' })
+    .send({ name: 'Walter Mitty', mail, passwd: '12345' })
     .then(res => {
       expect(res.status).toBe(201)
       expect(res.body.name).toBe('Walter Mitty')
