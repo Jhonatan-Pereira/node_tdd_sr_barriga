a8a567180764155a04f135fb3695eb2b2b011f49
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-03-13 15:52:38 -0300

    feat: Não deve remover uma conta sem transação

--- src/services/account.js
+++ src/services/account.js
@@ -23,7 +23,11 @@ module.exports = (app) => {
     return app.db('accounts').where({ id }).update(account, '*')
   }
 
-  const remove = (id) => {
+  const remove = async (id) => {
+
+    const transaction = await app.services.transaction.findOne({ acc_id: id })
+    if(transaction) throw new ValidatorError('Essa conta possui transações associadas')
+
     return app.db('accounts').where({ id }).del()
   }
 
