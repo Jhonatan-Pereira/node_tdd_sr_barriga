4b12c67dc5a6d173e91f216a92293b017f7c2bf7
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2021-03-13 18:10:28 -0300

    feat: Deve inserir uma transferência com sucesso

--- src/routes/transfers.js
+++ src/routes/transfers.js
@@ -8,6 +8,13 @@ module.exports = (app) => {
       .then(result => res.status(200).json(result))
       .catch(err => next(err))
   })
+  
+  router.post('/', (req, res, next) => {
+    const transfer = { ...req.body, user_id: req.user.id }
+    app.services.transfer.save(transfer)
+      .then(result => res.status(201).json(result[0]))
+      .catch(err => next(err))
+  })
 
   return router;
 }
